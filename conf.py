b8d3195dbd5b886ae29e074a394c351dcc01f31f
Ezequiel Leonardo Castaño <elcg@gmx.com>
2017-12-14 15:11:39 -0300

    Add features: Gravatar, Neighbors, Related and Update Theme

--- conf.py
+++ conf.py
@@ -8,20 +8,23 @@ sys.path.append(os.curdir)
 
 # Server
 
-SITEURL = 'https://elc.github.io/'
+SITEURL = 'https://elc.github.io'
 RELATIVE_URLS = False
-AUTHOR = 'Ezequiel Leonardo Castaño'
-SITENAME = 'Ezequiel Leonardo Castaño'
+AUTHOR = 'Ezequiel Castaño'
+AUTHOR_EMAIL = 'skielcast@gmail.com'
+AUTHOR_AVATAR = "https://s.gravatar.com/avatar/a8cb2c3394122f262cbb143e700b1beb?s=80"
+SITENAME = 'ELCWeb'
 DELETE_OUTPUT_DIRECTORY = True
 PATH = 'content'
 TIMEZONE = 'America/Argentina/Buenos_Aires'
 PAGE_ORDER_BY = 'order'
 
+RELATED_POSTS_MAX = 3
 
 # Plugins
 
 PLUGIN_PATHS = ['plugins']
-PLUGINS = ['pelican-ert', 'backreftranslate', 'summary']
+PLUGINS = ['pelican-ert', 'backreftranslate', 'summary', 'gravatar', 'neighbors', 'related_posts']
 
 # ERT
 
@@ -45,11 +48,29 @@ DISPLAY_CATEGORIES_ON_MENU = False
 ARTICLE_URL = 'posts/{slug}'
 ARTICLE_SAVE_AS = 'posts/{slug}/index.html'
 
+ARCHIVES_URL = 'archives/'
+ARCHIVES_SAVE_AS = 'archives/index.html'
+
 PAGE_URL = 'pages/{slug}/'
 PAGE_SAVE_AS = 'pages/{slug}/index.html'
 
 YEAR_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/index.html'
 MONTH_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/{date:%b}/index.html'
+
+TAG_URL = 'tag/{slug}/'
+TAG_SAVE_AS = 'tag/{slug}/index.html'
+TAGS_URL = 'tags/'
+TAGS_SAVE_AS = 'tags/index.html'
+
+AUTHOR_URL = 'author/{slug}/'
+AUTHOR_SAVE_AS = 'author/{slug}/index.html'
+AUTHORS_URL = 'authors/'
+AUTHORS_SAVE_AS = 'authors/index.html'
+
+CATEGORY_URL = 'category/{slug}/'
+CATEGORY_SAVE_AS = 'category/{slug}/index.html'
+CATEGORYS_URL = 'categories/'
+CATEGORYS_SAVE_AS = 'categories/index.html'
 
 # Blogroll 
 
@@ -66,3 +87,8 @@ SOCIAL = (
 # Pagination
 
 DEFAULT_PAGINATION = 10
+
+PAGINATION_PATTERNS = (
+    (1, '{base_name}/', '{base_name}/index.html'),
+    (2, '{base_name}/page/{number}/', '{base_name}/page/{number}/index.html'),
+)
