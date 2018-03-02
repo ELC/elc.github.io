7e4514c655de8bdff5991657d755f9ca0e0ddefe
Ezequiel Leonardo Castaño <elcg@gmx.com>
2018-03-02 11:22:11 -0300

    Use invoke inside Travis

--- tasks.py
+++ tasks.py
@@ -2,4 +2,8 @@ from invoke import task, run
 
 @task
 def build(ctx):
+    ctx.run("pelican -s conf.py -o output -t theme content")
+
+@task
+def test(ctx):
     ctx.run("pelican -s devconf.py -o output -t theme content")
