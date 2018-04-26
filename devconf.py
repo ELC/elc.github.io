4271088568704a0029dfc352d09dc74aba8e3922
Ezequiel Leonardo Castaño <elcg@gmx.com>
2018-04-26 13:18:41 -0300

    :building_construction: Clone Repository in CI instead of using Submodules

---
+++ devconf.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+from __future__ import unicode_literals
+import os
+import sys
+sys.path.append(os.curdir)
+from conf import *
+
+SITEURL = 'https://elcweb.netlify.com'
