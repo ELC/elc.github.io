9a73169400ae2c9e4524974dc770019dd2e9dcc0
Ezequiel Leonardo Castaño <elcg@gmx.com>
2018-10-04 14:55:19 -0300

    :wrench: Improve testing environment on Windows

--- tasks.py
+++ tasks.py
@@ -1,10 +1,16 @@
 from invoke import task
+from time import time
+import datetime
 
 @task
 def buildWin(c):
+    print('Started - {:%H:%M:%S}'.format(datetime.datetime.now()))
+    start_time = time()
     c.run("pipenv run copy-windows")
     c.run("pipenv run compile-css")
     c.run("pipenv run test")
+    end_time = time()
+    print('Elapsed Time: {:.2f} seconds'.format(end_time - start_time))
 
 @task
 def buildTheme(c):
