4db290f129b8ba48550f5e0b19bb8b00ce5a1207
Ezequiel Leonardo Castaño <elcg@gmx.com>
2018-10-10 18:02:39 -0300

    🚀 Compile all SCSS files in folder with compression

--- tasks.py
+++ tasks.py
@@ -1,13 +1,20 @@
 from invoke import task
 from time import time
 import datetime
+from cache import get_filepaths
+
+def compile_scss(c, path):
+    paths = get_filepaths(path, 'scss')
+    for filename in paths:
+        c.run(f"sass {filename} {filename}.css -s compressed")
 
 @task
 def buildWin(c):
     print('Started - {:%H:%M:%S}'.format(datetime.datetime.now()))
     start_time = time()
+    c.run("rd theme /S /Q ")
     c.run("pipenv run copy-windows")
-    c.run("pipenv run compile-css")
+    compile_scss(c, 'theme')
     c.run("pipenv run test")
     c.run("pipenv run clean")
     end_time = time()
@@ -16,13 +23,13 @@ def buildWin(c):
 @task
 def buildTheme(c):
     c.run("pipenv run copy-windows")
-    c.run("pipenv run compile-css")
+    compile_scss(c, 'theme')
     c.run("pipenv run test-theme")
     c.run("pipenv run clean")
 
 @task
 def build(c):
     c.run("pipenv run download")
-    c.run("pipenv run compile-css")
+    compile_scss(c, 'theme')
     c.run("pipenv run build")
     c.run("pipenv run clean")
