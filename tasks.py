eaae64a8fbeef501ba8acab47d43c78e5e376c85
Ezequiel Leonardo Castaño <elcg@gmx.com>
2019-01-15 23:34:29 -0300

    :recycle: Use 4 space Identation

--- tasks.py
+++ tasks.py
@@ -5,35 +5,35 @@ import os
 
 # Function extracted from https://stackoverflow.com/a/19308592/7690767
 def get_filepaths(directory, extensions=[], ignores=[]): 
-        """
-        This function will generate the file names in a directory 
-        tree by walking the tree either top-down or bottom-up. For each 
-        directory in the tree rooted at directory top (including top itself), 
-        it yields a 3-tuple (dirpath, dirnames, filenames).
-        """
-        file_paths = []  # List which will store all of the full filepaths.
-        
-        exts = extensions
+    """
+    This function will generate the file names in a directory 
+    tree by walking the tree either top-down or bottom-up. For each 
+    directory in the tree rooted at directory top (including top itself), 
+    it yields a 3-tuple (dirpath, dirnames, filenames).
+    """
+    file_paths = []  # List which will store all of the full filepaths.
+    
+    exts = extensions
 
-        if isinstance(extensions, str):
-            exts = [extensions]
+    if isinstance(extensions, str):
+        exts = [extensions]
 
-        igns = ignores
+    igns = ignores
 
-        if isinstance(ignores, str):
-            igns = [ignores]
+    if isinstance(ignores, str):
+        igns = [ignores]
 
-        # Walk the tree.
-        for root, directories, files in os.walk(directory):
-            for filename in files:
+    # Walk the tree.
+    for root, directories, files in os.walk(directory):
+        for filename in files:
 
-                if filename in igns or not any(filename.endswith(f".{ext}") for ext in exts):
-                    continue
+            if filename in igns or not any(filename.endswith(f".{ext}") for ext in exts):
+                continue
 
-                filepath = os.path.join(root, filename).replace("\\", "/")
-                file_paths.append(filepath)
+            filepath = os.path.join(root, filename).replace("\\", "/")
+            file_paths.append(filepath)
 
-        return file_paths
+    return file_paths
 
 def compile_scss(c, path):
     paths = get_filepaths(path, 'scss')
