84d02f8048351a15c83e707248fe08a1541e4a89
Ezequiel Leonardo Castaño <elcg@gmx.com>
2019-08-09 21:58:59 -0300

    :recycle: Refactor Shortener

--- plugins/shortener/shortener.py
+++ plugins/shortener/shortener.py
@@ -18,29 +18,6 @@ import json
 def generate_output(sender):
     """ Generates a directory structure with the HTML files necessary for the redirects """
 
-    BASE_HTML = """
-    <html>
-    <head>
-    <meta http-equiv="refresh" content="0; URL='{0}'" />
-    </head>
-    <body>
-    {analytics}
-    </body>
-    </html>
-    """
-
-    ANALYTICS = """
-    <!-- Google Analytics -->
-    <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-        ga('create', 'GA_CODE', 'auto');
-        ga('send', 'pageview');
-    </script>
-    """
-
     output_path = getattr(sender, 'output_path', None)
 
     redirects_map = sender.settings.get('SHORTENER_LINKS', None)
@@ -65,57 +42,60 @@ def generate_output(sender):
     if not redirects_folder is None:
         output_path_with_folder = os.path.join(output_path, redirects_folder)
 
-    if analytics is None or analytics == "":
-        ANALYTICS = ""
-
-    process_later = {}
-
     for filename, redirect_url in redirects_map.items():
 
         if ignore_folder and filename in ignore_folder:
-            process_later[filename] = redirect_url
+            generate_folder_file(filename, output_path, redirect_url, analytics)
 
-        if not (redirect_url.startswith('http://') or redirect_url.startswith('https://')):
-            redirect_url = 'http://' + redirect_url
+        generate_folder_file(filename, output_path_with_folder, redirect_url, analytics)
+
+def generate_folder_file(file, folder, link, analytics):
+
+    BASE_HTML = """
+    <html>
+    <head>
+    <meta http-equiv="refresh" content="0; URL='{0}'" />
+    </head>
+    <body>
+    {analytics}
+    </body>
+    </html>
+    """
+
+    ANALYTICS = """
+    <!-- Google Analytics -->
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+        ga('create', 'GA_CODE', 'auto');
+        ga('send', 'pageview');
+    </script>
+    """
+
+    if analytics is None or analytics == "":
+        ANALYTICS = ""
+
+    if not (link.startswith('http://') or link.startswith('https://')):
+        link = 'http://' + link
+    
+    folder_path = os.path.join(folder, file)
+
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
+    file_path = os.path.join(folder_path, 'index.html')
+
+    with open(file_path, 'w', encoding='utf-8') as fd:
         
-        folder_path = os.path.join(output_path_with_folder, filename)
+        if not analytics is None:
+            analytics_snipped = ANALYTICS.replace('GA_CODE', analytics)
+            content = BASE_HTML.format(link, analytics=analytics_snipped)
+        else:
+            content = BASE_HTML.format(link, analytics="")
 
-        if not os.path.exists(folder_path):
-            os.makedirs(folder_path)
-
-        file_path = os.path.join(folder_path, 'index.html')
-
-        with open(file_path, 'w', encoding='utf-8') as fd:
-            
-            if not analytics is None:
-                analytics_snipped = ANALYTICS.replace('GA_CODE', analytics)
-                content = BASE_HTML.format(redirect_url, analytics=analytics_snipped)
-            else:
-                content = BASE_HTML.format(redirect_url, analytics="")
-
-            fd.write(content)
-
-    for filename, redirect_url in process_later.items():
-
-        if not (redirect_url.startswith('http://') or redirect_url.startswith('https://')):
-            redirect_url = 'http://' + redirect_url
-        
-        folder_path = os.path.join(output_path, filename)
-
-        if not os.path.exists(folder_path):
-            os.makedirs(folder_path)
-
-        file_path = os.path.join(folder_path, 'index.html')
-
-        with open(file_path, 'w', encoding='utf-8') as fd:
-            
-            if not analytics is None:
-                analytics_snipped = ANALYTICS.replace('GA_CODE', analytics)
-                content = BASE_HTML.format(redirect_url, analytics=analytics_snipped)
-            else:
-                content = BASE_HTML.format(redirect_url, analytics="")
-
-            fd.write(content)
+        fd.write(content)
 
 
 def register():
