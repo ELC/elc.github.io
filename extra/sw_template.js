3ff1a0d92b3e1020bcea8f29c3f09f95bdd1e68a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-09 18:05:42 +0000

    Deploy elc/elc.github.io to github.com/elc/elc.github.io.git:master

---
+++ extra/sw_template.js
@@ -0,0 +1,35 @@
+const version = "$VERSION$";
+const currentCacheName = `ELCWEB-${version}`;
+const filesToCache = "$FILES_TO_CACHE$"
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(currentCacheName)
+      .then(cache => cache.addAll(filesToCache))
+      .then(self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(cacheNames => cacheNames.filter(cacheName => ! currentCacheName.includes(cacheName) ))
+      .then(cachesToDelete => Promise.all(cachesToDelete.map(cacheToDelete => caches.delete(cacheToDelete))))
+      .then(self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  const url = event.request.url;
+  const scope = self.registration.scope;
+  
+	if (!url.startsWith(scope)) {
+		return;
+  }
+
+  event.respondWith(
+    caches.open(currentCacheName)
+      .then(cache => cache.match(event.request, {ignoreSearch: true}) )
+      .then(response => response || fetch(event.request) )
+  );
+});
