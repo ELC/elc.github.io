d9c40e460bb9d24b82d70fe088008f7530379850
ELC <ELC@users.noreply.github.com>
2020-12-16 03:51:37 +0000

    deploy: 00c4a45eb0e893b436f6bf0c2b58bdf76077d37f

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const version = "76cc68c";
+const version = "aaf6050";
 const currentCacheName = `ELCWEB-${version}`;
 const filesToCache = [
     "/?v=0a7829a",
@@ -254,7 +254,7 @@ const filesToCache = [
     "/research/?v=a28eabb",
     "/safari-pinned-tab.svg?v=4686042",
     "/site.webmanifest?v=7ba4ab1",
-    "/sitemap.xml?v=f7f9dca",
+    "/sitemap.xml?v=eebd20a",
     "/tabubiblico/?v=4d48021",
     "/tabubiblico/android-chrome-192x192.png?v=26f8443",
     "/tabubiblico/android-chrome-384x384.png?v=cedeeac",
